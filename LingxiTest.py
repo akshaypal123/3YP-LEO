fbc7dad195a4a2c683557d11d89423072ed3b287
ZeLinx33 <38849678+ZeLinx33@users.noreply.github.com>
2020-11-02 09:42:36 +0000

    Update and rename LingxiTest to LingxiTest.py

---
+++ LingxiTest.py
@@ -0,0 +1,7 @@
+print("Hello, World")     #prints text "Hello, World"
+
+x = input("Enter x:")
+y = input("Enter y:')
+sum = float(x) + float(y)   #sums up x and y 
+print ("The result is {1}.format(sum))
+
